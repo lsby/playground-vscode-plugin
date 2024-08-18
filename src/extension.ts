a5e32798df74b7013628501f370ed9c1720bca56
hbybyyang <2451759073@qq.com>
2024-08-18 22:57:09 +0800

    整理

--- src/extension.ts
+++ src/extension.ts
@@ -5,8 +5,6 @@ var 插件名称 = 'playground-vscode-plugin'
 export function activate(context: vscode.ExtensionContext): void {
   console.log(`${插件名称}: 插件开始运行`)
 
-  void vscode.window.showInformationMessage(`Hello World!`)
-
   context.subscriptions.push(
     vscode.commands.registerCommand(`${插件名称}.helloWorld`, async () => {
       await vscode.window.showInformationMessage(`Hello World!`)
